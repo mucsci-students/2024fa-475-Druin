45bb2ef5776951547d5d4380411e878dab1058fe
jrlease-03 <jrlease@millersville.edu>
2024-10-28 10:46:09 -0400

    battle manager scripts setup

---
+++ Druin/Assets/Scripts/BattleManager/MenuAbstract.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Menu : MonoBehaviour
+{
+    //set up variables here for classes that inherit this one
+
+    // Start is called before the first frame update
+    public abstract void Start();
+
+    // Update is called once per frame
+    public abstract void Update();
+}
