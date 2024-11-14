7e1608a109751c890e398ced06b11faad7896936
Mia Joseph <miashiojos@gmail.com>
2024-11-14 02:39:25 -0500

    Pause/Inv UI is functional

--- Druin/Assets/Scripts/PauseMenu.cs
+++ Druin/Assets/Scripts/PauseMenu.cs
@@ -8,12 +8,17 @@ public class PauseMenu : MonoBehaviour
     public GameObject pauseMenuUI; // Reference to the Pause Menu panel
     private bool isPaused = false; // Keeps track of pause state
     private GameObject dialoguePanel;
+    public GameObject settingsBackground;
+    public GameObject quitPrompt;
 
     void Update()
     {
         if (SceneManager.GetActiveScene().name != "BattleScene"
         && Input.GetKeyDown(KeyCode.Escape)
-        && (dialoguePanel == null || !dialoguePanel.activeSelf)) // Detect ESC key press
+        && (dialoguePanel == null || !dialoguePanel.activeSelf) // Detect ESC key press
+        && (dialoguePanel == null || !dialoguePanel.activeSelf)
+        && !settingsBackground.activeSelf
+        && !quitPrompt.activeSelf) // Detect ESC key press
         {
             if (isPaused)
             {
@@ -26,11 +31,18 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
+    public bool IsGamePaused()
+    {
+        return isPaused;
+    }
+
     public void PauseGame()
     {
         Debug.Log("Pausing game");
         pauseMenuUI.SetActive(true); 
         Time.timeScale = 0f;         
+        pauseMenuUI.SetActive(true);    
+        Time.timeScale = 0.0001f;
         isPaused = true;
     }
 
